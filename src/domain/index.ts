58015294b108e2d8fffa8f3f1d9c4f9e24271f21
Erick Silva <erick.silva6653@gmail.com>
2026-07-23 10:55:34 -0300

    feat: port transaction validation schema

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,5 +1,6 @@
 export * from './categories';
 export * from './constants';
+export * from './schema';
 export * from './suggestCategory';
 export * from './transaction';
 export * from './transactions';
